8253b82260a53904dafd4616938d6a3904db7e03
Himanshu-0076 <ash123master@gmail.com>
2026-06-14 01:24:59 +0530

    Add profile section and more in the profile section

--- project2/app.js
+++ project2/app.js
@@ -19,9 +19,46 @@ app.get("/login", (req, res)=> {
     res.render("login")
 })
 
-app.get("/profile",isLoggedIn, (req, res)=> {
-    console.log(req.user)
-    res.render("login")
+app.get("/profile",isLoggedIn, async(req, res)=> {
+    let user = await userModel.findOne({email: req.user.email}).populate("posts")
+    res.render("profile", {user})
+})
+
+app.get("/like/:id",isLoggedIn, async(req,res)=> {
+    let post = await postModel.findOne({_id: req.params.id}).populate("user")
+     
+    if(post.likes.indexOf(req.user.userid) === -1){
+        post.likes.push(req.user.userid)
+    }
+    else{
+        post.likes.splice(post.likes.indexOf(req.user.userid), 1);
+    }
+
+    await post.save()                       
+    res.redirect("/profile")
+})
+
+app.get("/edit/:id", isLoggedIn, async(req, res)=> {
+    let post = await postModel.findOne({_id: req.params.id}).populate("user");
+    res.render("edit", {post})
+})
+
+app.post("/update/:id", isLoggedIn, async(req,res)=>{
+    let post = await postModel.findOneAndUpdate({_id: req.params.id}, {content: req.body.content})
+    res.redirect("/profile")
+})
+
+app.post("/post", isLoggedIn, async(req, res)=> {
+    let user = await userModel.findOne({email: req.user.email});
+    let {content} = req.body;
+    let post = await postModel.create({
+        user: user._id,
+        content
+    })
+
+    user.posts.push(post._id);
+    await user.save()
+    res.redirect("/profile")
 })
 
 app.post("/register", async(req,res)=> {
@@ -41,7 +78,7 @@ app.post("/register", async(req,res)=> {
 
             let token = jwt.sign({email: email, userid: user._id}, "shhhhhhhhhhh");
             res.cookie("token", token);
-            res.send("registration Done")
+            res.redirect("/profile")
         })
     })
 })
@@ -55,7 +92,7 @@ app.post("/login", async(req,res)=> {
         if(result) {
             let token = jwt.sign({email: email, userid: user._id}, "shhhhhhhhhhh");
             res.cookie("token", token);
-            res.status(200).send("You are login")
+            res.status(200).redirect("/profile")
         }
         else res.redirect("/login");
     })
@@ -67,7 +104,7 @@ app.get("/logout", (req, res)=> {
 })
 
 function isLoggedIn(req, res, next){
-    if(req.cookies.token === "") res.send("you must be Log In")
+    if(req.cookies.token === "") res.redirect("/login")
     else{
         let data = jwt.verify(req.cookies.token, "shhhhhhhhhhh")
         req.user = data;
