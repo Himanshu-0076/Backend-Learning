576992deca80c47b9b76e59bb26da05d7c19a6c0
Himanshu-0076 <ash123master@gmail.com>
2026-06-21 01:13:20 +0530

    creating a Bagify website in which login and logout routes are ready

---
+++ Bagify/controllers/authController.js
@@ -0,0 +1,51 @@
+const userModel = require("../models/userModel")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const {generateToken} = require("../utils/generateToken")
+
+module.exports.registerUser = async function (req, res){
+    try {
+        let { fullname, email, password } = req.body;
+
+        let user = await userModel.findOne({email: email})
+        if(user) return res.status(401).send("You already have an account, please login")
+        bcrypt.genSalt(10, function (err, salt) {
+            bcrypt.hash(password, salt, async function (err, hash) {
+                if (err) return res.send(err.message)
+                else {
+                    let user = await userModel.create({
+                        email: email,
+                        password: hash,
+                        fullname: fullname
+                    })
+                   let token = generateToken(user);
+                   res.cookie("token", token)
+                   res.send("user created successifully")
+                }
+            })
+        })
+        //     
+    }
+    catch (err) {
+        console.log(err.message)
+    }
+}
+
+
+module.exports.loginUser = async function(req,res){
+    let {email, password} = req.body;
+    let user = await userModel.findOne({email: email});
+
+    if(!user) return res.status(401).send("Eamil or Password incorrect.")
+
+    bcrypt.compare(password, user.password, function(err, result){
+        if(result){
+        let token = generateToken(user);
+        res.cookie("token", token);
+        res.send("you are login")
+        }
+        else {
+            res.send("Incorrect email or password")
+        }
+    })
+}
---
+++ Bagify/routes/usersRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {registerUser, loginUser} = require("../controllers/authController")
+
+router.get("/", (req, res) => {
+    res.send("user route working")
+})
+
+router.post("/register", registerUser)
+
+router.post("/login", loginUser)
+
+module.exports = router;
